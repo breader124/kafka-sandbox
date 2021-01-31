bce6ace6b925abf0292b3229fc8a0bf63de7dd8c
breader124 <breaderman@gmail.com>
2021-02-01 00:53:29 +0100

    implemented basic kafka stream application -- word counter, just for learning purposes

---
+++ kafka-streams-intermediate/src/main/java/udemy/breader/com/StreamsStarterApp.java
@@ -0,0 +1,46 @@
+package udemy.breader.com;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.*;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+public class StreamsStarterApp {
+    public static void main(String[] args) {
+        Properties config = new Properties();
+        config.setProperty(StreamsConfig.APPLICATION_ID_CONFIG, "word-count");
+        config.setProperty(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        config.setProperty(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        config.setProperty(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+
+        StreamsBuilder builder = new StreamsBuilder();
+
+        KStream<String, String> wordCountInput = builder.stream("word-count-input");
+
+        KTable<String, Long> wordCounts = wordCountInput
+                .mapValues((ValueMapper<String, String>) String::toLowerCase)
+                .flatMapValues(value -> Arrays.asList(value.split(" ")))
+                .selectKey((key, value) -> value)
+                .groupByKey()
+                .count(Named.as("Counts"));
+
+        KStream<String, Long> wordCountOutput = wordCounts.toStream();
+        wordCountOutput.to("word-count-output", Produced.with(Serdes.String(), Serdes.Long()));
+
+        KafkaStreams streams = new KafkaStreams(builder.build(), config);
+        streams.start();
+
+        // print the topology
+        System.out.println(streams.toString());
+
+        // gracefully shutdown an application
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+    }
+}
